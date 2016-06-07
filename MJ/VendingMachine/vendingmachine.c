4df9620b1f3942c781c84a154c577a70a0cb4a7a
Marcin <marcinjaj@gmail.com>
2016-06-07 23:13:59 +0200

    już są threadsy i subprogramy

---
+++ MJ/VendingMachine/vendingmachine.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+//3 tablice do produktów
+float[] productPrice;
+int[] productQuanity;
+string[] productName;
+
+//potrzebne do algorytmu wydawania reszty
+int[] coin;
+
+float paid;
+
+//miejsce na 7 subprogramów
